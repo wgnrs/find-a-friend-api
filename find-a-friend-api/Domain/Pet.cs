dd388f821127a447a985e7ce5ecb332a5ab55e66
Wagner <wagner_silvajr@hotmail.com>
2023-05-17 20:40:36 -0300

    feat: created entity Pet on Domain

---
+++ find-a-friend-api/Domain/Pet.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace find_a_friend_api.Domain
+{
+    public class Pet
+    {
+        
+    }
+}
